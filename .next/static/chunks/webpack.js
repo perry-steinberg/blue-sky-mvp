6d12a6d51f16effa6228bd201d0a56d5d8960fd3
perry-steinberg <perry_steinberg@yahoo.com>
2025-03-21 12:44:24 -0700

    fixed index.js not being recognized

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2782b5eb316e559a"; }
+/******/ 		__webpack_require__.h = function() { return "1f59c97f4f5467a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 export default function Home() {
     return (
       <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-        <h1>👋 Welcome to My Bluesky Frontend (updated)</h1>
+        <h1>👋 Welcome to My Bluesky Frontend (updated number2)</h1>
         <p>This is a simple Next.js page deployed with Vercel.</p>
       </div>
     );
   }
+
 
   
